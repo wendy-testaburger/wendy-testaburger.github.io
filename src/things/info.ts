f7e27f7fa6f2758dec2dd259cdb2025c809ea8f7
Kateryna <kateryna.yakimchuk@ukrainiansoftware.com>
2020-05-09 23:55:21 +0300

    add thing item

--- src/things/info.ts
+++ src/things/info.ts
@@ -4,8 +4,8 @@ const things = [
         title: "Мятная кофта",
         description: "Размер S-M. 80% viscose, 20% polyamide",
         price: 30,
-        brand: "O'STIN",
         size: "M",
+        brand: "O'STIN",
     }
 ];
 
--- src/views/main/components/thing-item/ThingItem.vue
+++ src/views/main/components/thing-item/ThingItem.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>
-        {{thing.title}}
-        {{thing.title}}
-        {{thing.title}}
-        <v-btn>pppppp</v-btn>   
-        
+    <div class="thing-item">
+        <div class="thing-item__image">
+            <img :src="require(`@/things/photos/${thing.id}/1.jpg`)">
+        </div>
+        <div class="thing-item__info">
+            <div class="thing-item__title center bold">
+                {{thing.title}}
+            </div>
+            <div class="thing-item__brand">
+                <span class="bold">Бренд:</span>
+                {{thing.brand}}
+            </div>
+            <div class="d-flex justify-space-between">
+                <div class="thing-item__size">
+                    <span class="bold">Размер:</span>
+                    {{thing.size}}
+                </div>
+                <div class="thing-item__price">
+                    <span class="bold">Цена:</span>
+                    {{thing.price}} грн.
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -21,5 +38,31 @@ export default class ThingItem extends Vue {
 </script>
 
 <style lang="scss">
+    .thing-item {
+        width: 300px;
+        border-radius: $border-radius;
+        background-color: $bg-card;
+        // padding: 10px;
 
+        &__image {
+            img {
+                border-top-left-radius: $border-radius;
+                border-top-right-radius: $border-radius;
+                max-width: 100%;
+            }
+        }
+
+        &__info {
+            padding: 10px 20px;
+
+            &>div {
+                margin-bottom: 10px;
+            }
+        }
+
+
+        // &>div:nth-child(n+3) {
+        //     margin-left: 20px;
+        // }
+    }
 </style>
