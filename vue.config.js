ed303e638494c242545fdba15f64466b9d80d4ce
Kateryna <kateryna.yakimchuk@ukrainiansoftware.com>
2020-05-10 00:35:55 +0300

    change config

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
             prependData: ` @import "@/assets/styles/_variables.scss";`
         }
       }
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/wendy-testaburger.github.io/'
+    : '/'
   };
